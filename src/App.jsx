b2af7291261c51762c570aef03f8ef4d424146af
PauloSilvaRamalho <phsilvarm@gmail.com>
2026-05-25 17:02:07 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useRef } from 'react';
+import './App.css';
 import s from './app.module.css';
 import { Card } from './components/cards';
 import { CardEsp } from './components/cards';
@@ -9,15 +9,25 @@ import cardImg2 from "/img2.jpg";
 
 function App() {
   const [count, setCount] = useState(0)
+  const scrollRef = useRef(null);
 
   return (
     <>
-      <nav>Theoros</nav>
+     <nav>Theoros
+      <a href="menu">menu</a>
+     </nav>
+    <main>
+      <div className={s.maincont}>
       <div className={s.container}>
-        <section id="s2" className={s.s2}>
-          <Card imgSrc={"/img1.jpg"} alt="Card img1" text="Lorem Ipsum..." title="Card 1" />
+        <section id="s2" className={s.s2} ref={scrollRef}>
+          {/* <button className="seta-esq" onClick={() => scrollRef.current?.scrollBy({ left: -300, behavior: 'smooth' })}>◀</button> */}
+          <Card imgSrc={"/img1.jpg"} alt="Card img1" text="Lorem Ipsum..." title="Card 1"className={s.cartaum}/>
+          {/* <button className="seta-dir" onClick={() => scrollRef.current?.scrollBy({ left: 300, behavior: 'smooth' })}>▶</button> */}
         </section>
         </div>
+        
+
+
         <div className={s.containerdois}>
         <section id="s3" className={s.s3}>
           <CardEsp imgSrc={"/img2.jpg"} alt="Card img2" title="Card 2" />
@@ -30,10 +40,14 @@ function App() {
         </section>    
         <section id="s3" className={s.s3}>
           <CardEsp imgSrc={"/img2.jpg"} alt="Card img2" title="Card 2" />
-        </section>    
+        </section>
+        
+            
       </div>
+      </div>
+      </main>
     </>
   )
-}
+} 
 
 export default App
