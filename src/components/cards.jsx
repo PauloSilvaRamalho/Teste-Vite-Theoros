363bc6df1d09e912ec143b05124f6b5a0a15fe9e
PauloSilvaRamalho <phsilvarm@gmail.com>
2026-05-31 20:33:13 -0300

    ui

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -5,6 +5,7 @@ export const Card = ({imgSrc, alt, text, title}) => {
     return (
         <a href="https://www.youtube.com/watch?v=uuVaiMk8-pk" className={s.card}>
             <img src={imgSrc} alt={alt} />
+            
             <div className={s.cardContent}>
             <p className={s.cardTitle}>{title}</p>
             <p>{text?.substring(0, 300)}...</p>
